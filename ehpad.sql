0cd74e65af9311e9e79ec9d55799a86afe7ce383
Pickendorf <seal.dofus@gmail.com>
2022-12-09 15:48:34 +0000

    Add files via upload
    Moins de requêtes et create user

--- ehpad.sql
+++ ehpad.sql
@@ -197,251 +197,119 @@ CREATE TABLE doit_administrer(
 
 
 -- DEBUT DES VALEURS DE TEST
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('HADDOCK', 'Archibald', '1920-06-01', CURDATE(), 'archibald.haddock@lemans.fr', '+33659847264');
-
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('ISTIS', 'Larme', '1918-11-11', CURDATE(), 'larme.istis@pastis.fr', '+33688888888');
-
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('TERIEUR', 'Alex', '1932-12-24', CURDATE(), 'papa.noel@pole-nord.com', '+3966598482530');
-
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('TERIEUR', 'Alain', '1938-02-14', CURDATE(), 'cupidon@love.com', '+33662984715');
-
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('CURRY', 'Marie', '1960-09-21', CURDATE(), 'marieeaucurry@infirmiere.fr', '+33666666666');
-
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('BRETON', 'Anna', '1991-11-11', CURDATE(), 'bretagnelibre@insoumis.fr', '+33699999999');
-
-INSERT INTO personne(nom, prenom, date_naissance, date_arrivee, email, telephone)
-VALUES ('BEBOU', 'Amelie', '1975-05-05', CURDATE(), 'bebou@docteur.fr', '+33611111111');
-
-INSERT INTO etage(etage_securise)
-VALUES (false);
-
-INSERT INTO etage(etage_securise)
-VALUES (true);
-
-INSERT INTO etage(etage_securise)
-VALUES (false);
-
-INSERT INTO etage(etage_securise)
-VALUES (false);
-
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (201, false, 2);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (202, false, 2);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (203, false, 2);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (204, false, 2);
-
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (301, false, 3);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (302, true, 3);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (303, true, 3);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (304, false, 3);
-
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (401, false, 4);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (402, true, 4);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (403, true, 4);
-INSERT INTO chambre(no_chambre, chambre_double, id_etage)
-VALUES (404, false, 4);
-
-INSERT INTO pensionnaire(no_secu, id_chambre, id_personne)
-VALUES (120069135923302, 1, 1);
-
-INSERT INTO pensionnaire(no_secu, id_chambre, id_personne)
-VALUES (118119135923555, 2, 2);
-
-INSERT INTO pensionnaire(no_secu, id_chambre, id_personne)
-VALUES (132069135923666, 4, 3);
-
-INSERT INTO pensionnaire(no_secu, id_chambre, id_personne)
-VALUES (138069135923777, 3, 4);
-
-INSERT INTO fonction(nom)
-VALUES ('infirmiere');
-
-INSERT INTO fonction(nom)
-VALUES ('cuisinier');
-
-INSERT INTO fonction(nom)
-VALUES ("personnel_administratif");
-
-INSERT INTO fonction(nom)
-VALUES ('comptable');
-
-INSERT INTO employe(no_secu, id_fonction, id_personne)
-VALUES (160099135923888, 1, 5);
-
-INSERT INTO infirmiere(no_RPPS, id_employe)
-VALUES (12345678901, 1);
-
-INSERT INTO medicament(nom, fonction, stock)
-VALUES ("Doliprane", 'antalgique', 50);
-
-INSERT INTO medicament(nom, fonction, stock)
-VALUES ("Aspegic", 'antalgique', 80);
-
-INSERT INTO medicament(nom, fonction, stock)
-VALUES ("céphalexine", 'antibiotique', 5);
-
-INSERT INTO medecin(id_personne)
-VALUES (7);
-
-INSERT INTO ordonnance(id_medecin, id_pensionnaire, jour)
-VALUES (1,3, CURDATE());
-
-INSERT INTO prescription(id_medicament, id_medecin, id_pensionnaire, jour, posologie, date_debut_traitement, date_fin_traitement)
-VALUES (1, 1,3, CURDATE(), "- Paracétamol 1000mg : 3 comprimés par jour", curdate(), '2023-07-14');
-
-INSERT INTO doit_administrer(id_medecin, id_pensionnaire, jour, no_RPPS)
-VALUES (1, 3, CURDATE(), 12345678901);
-
-INSERT INTO allergene(nom)
-VALUES ("gluten");
-
-INSERT INTO allergene(nom)
-VALUES ("soja");
-
-INSERT INTO allergene(nom)
-VALUES ("arachide");
-
-INSERT INTO allergene(nom)
-VALUES ("lait");
-
-INSERT INTO allergene(nom)
-VALUES ("moutarde");
-
-INSERT INTO allergene(nom)
-VALUES ("céleri");
-
-INSERT INTO plat(nom)
-VALUES ("steak-frites");
-
-INSERT INTO plat(nom)
-VALUES ("moules-frites");
-
-INSERT INTO plat(nom)
-VALUES ("tomates farcies");
-
-INSERT INTO plat(nom)
-VALUES ("lasagnes");
-
-INSERT INTO plat(nom)
-VALUES ("couscous");
-
-INSERT INTO plat(nom)
-VALUES ("purée cabillaud");
-
-INSERT INTO plat(nom)
-VALUES ("pâtes carbonara");
-
-INSERT INTO plat(nom)
-VALUES ("gratin de brocolis");
-
-INSERT INTO plat(nom)
-VALUES ("salade de pâtes");
-
-INSERT INTO plat(nom)
-VALUES ("chèvre chaud");
-
-INSERT INTO plat(nom)
-VALUES ("gaspacho");
-
-INSERT INTO plat(nom)
-VALUES ("tomate mozzarella");
-
-INSERT INTO plat(nom)
-VALUES ("velouté de légumes");
-
-INSERT INTO plat(nom)
-VALUES ("mousse au chocolat");
-
-INSERT INTO plat(nom)
-VALUES ("salade de fruits");
-
-INSERT INTO plat(nom)
-VALUES ("tarte aux poires");
-
-INSERT INTO plat(nom)
-VALUES ("tiramisu");
-
-INSERT INTO plat(nom)
-VALUES ("fromage blanc");
-
-INSERT INTO plat(nom)
-VALUES ("crumble aux pommes");
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2022-12-26');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2022-12-26');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2022-12-26');
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2022-12-27');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2022-12-27');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2022-12-27');
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2022-12-28');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2022-12-28');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2022-12-28');
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2022-12-29');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2022-12-29');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2022-12-29');
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2022-12-30');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2022-12-30');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2022-12-30');
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2022-12-31');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2022-12-31');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2022-12-31');
-
-INSERT INTO repas(rang, jour)
-VALUES (1, '2023-01-01');
-
-INSERT INTO repas(rang, jour)
-VALUES (2, '2023-01-01');
-
-INSERT INTO repas(rang, jour)
-VALUES (3, '2023-01-01');
+INSERT INTO personne(id_personne, nom, prenom, date_naissance, date_arrivee, email, telephone) VALUES 
+    (1, 'HADDOCK', 'Archibald', '1920-06-01', CURDATE(), 'archibald.haddock@lemans.fr', '+33659847264'),
+	(2, 'ISTIS', 'Larme', '1918-11-11', CURDATE(), 'larme.istis@pastis.fr', '+33688888888'),
+    (3, 'TERIEUR', 'Alex', '1932-12-24', CURDATE(), 'papa.noel@pole-nord.com', '+3966598482530'),
+    (4, 'TERIEUR', 'Alain', '1938-02-14', CURDATE(), 'cupidon@love.com', '+33662984715'),
+    (5, 'CURRY', 'Marie', '1960-09-21', CURDATE(), 'marieeaucurry@infirmiere.fr', '+33666666666'),
+    (6, 'BRETON', 'Anna', '1991-11-11', CURDATE(), 'bretagnelibre@insoumis.fr', '+33699999999'),
+    (7, 'BEBOU', 'Amelie', '1975-05-05', CURDATE(), 'bebou@docteur.fr', '+33611111111');
+
+		INSERT INTO etage(etage_securise)
+		VALUES (false),
+		(true),
+		(false),
+		(false);
+
+		INSERT INTO chambre(no_chambre, chambre_double, id_etage)
+		VALUES (201, false, 2),
+		(202, false, 2),
+		(203, false, 2),
+		(204, false, 2);
+
+		INSERT INTO chambre(no_chambre, chambre_double, id_etage)
+		VALUES (301, false, 3),
+		(302, true, 3),
+		(303, true, 3),
+		(304, false, 3),
+		(401, false, 4),
+		(402, true, 4),
+		(403, true, 4),
+		(404, false, 4);
+
+		INSERT INTO pensionnaire(no_secu, id_chambre, id_personne)
+		VALUES (120069135923302, 1, 1),
+		(118119135923555, 2, 2),
+		(132069135923666, 4, 3),
+		(138069135923777, 3, 4);
+
+		INSERT INTO fonction(nom)
+		VALUES ('infirmiere'),
+		('cuisinier'),
+		("personnel_administratif"),
+		('comptable');
+
+		INSERT INTO employe(no_secu, id_fonction, id_personne)
+		VALUES (160099135923888, 1, 5);
+
+		INSERT INTO infirmiere(no_RPPS, id_employe)
+		VALUES (12345678901, 1);
+
+		INSERT INTO medicament(nom, fonction, stock)
+		VALUES ("Doliprane", 'antalgique', 50),
+		("Aspegic", 'antalgique', 80),
+		("céphalexine", 'antibiotique', 5);
+
+		INSERT INTO medecin(id_personne)
+		VALUES (7);
+
+		INSERT INTO ordonnance(id_medecin, id_pensionnaire, jour)
+		VALUES (1,3, CURDATE());
+
+		INSERT INTO prescription(id_medicament, id_medecin, id_pensionnaire, jour, posologie, date_debut_traitement, date_fin_traitement)
+		VALUES (1, 1,3, CURDATE(), "- Paracétamol 1000mg : 3 comprimés par jour", curdate(), '2023-07-14');
+
+		INSERT INTO doit_administrer(id_medecin, id_pensionnaire, jour, no_RPPS)
+		VALUES (1, 3, CURDATE(), 12345678901);
+
+		INSERT INTO allergene(nom)
+		VALUES ("gluten"),
+		("soja"),
+		("arachide"),
+		("lait"),
+		("moutarde"),("céleri");
+
+		INSERT INTO plat(nom)
+		VALUES ("steak-frites"),
+		("moules-frites"),
+		("tomates farcies"),
+		("lasagnes"),
+		("couscous"),
+		("purée cabillaud"),
+		("pâtes carbonara"),
+		("gratin de brocolis"),
+		("salade de pâtes"),
+		("chèvre chaud"),
+		("gaspacho"),
+		("tomate mozzarella"),
+		("velouté de légumes"),
+		("mousse au chocolat"),
+		("salade de fruits"),
+		("tarte aux poires"),
+		("tiramisu"),
+		("fromage blanc"),
+		("crumble aux pommes");
+
+		INSERT INTO repas(rang, jour)
+		VALUES (1, '2022-12-26'),
+		(2, '2022-12-26'),
+		(3, '2022-12-26'),
+		(1, '2022-12-27'),
+		(2, '2022-12-27'),
+		(3, '2022-12-27'),
+		(1, '2022-12-28'),
+		(2, '2022-12-28'),
+		(3, '2022-12-28'),
+		(1, '2022-12-29'),
+		(2, '2022-12-29'),
+		(3, '2022-12-29'),
+		(1, '2022-12-30'),
+		(2, '2022-12-30'),
+		(3, '2022-12-30'),
+		(1, '2022-12-31'),
+		(2, '2022-12-31'),
+		(3, '2022-12-31'),
+		(1, '2023-01-01'),
+		(2, '2023-01-01'),
+		(3, '2023-01-01');
